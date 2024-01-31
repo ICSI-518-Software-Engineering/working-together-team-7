a92395563dce3b3c36ee7adff9859ede6f719186
TARUNSAI185 <tarunsaiyerragunta2000@gmail.com>
2024-01-31 14:04:22 -0500

    Implementing subtraction function

--- calculator.js
+++ calculator.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 // Multiplication
 function multiply(a, b) {
     // return a * b;
@@ -8,11 +9,27 @@ function add(a, b) {
     // return a + b;
 }
 
+=======
+>>>>>>> 31d85176b79294c5f38ad557aa605cd18e9d60c2
 // Subtraction
 function subtract(a, b) {
     // return a - b;
 }
 
+<<<<<<< HEAD
+=======
+// Addition
+function add(a, b) {
+     return a + b;
+}
+
+
+// Multiplication
+function multiply(a, b) {
+     return a * b;
+}
+
+>>>>>>> 31d85176b79294c5f38ad557aa605cd18e9d60c2
 // Division
 function divide(a, b) {
     if (b !== 0) {
@@ -24,6 +41,9 @@ function divide(a, b) {
 
 // Examples
 let resultAddition = add(5, 3);
+let resultAddition1 = add(6, 3);
+let resultAddition2 = add(7, 3);
+
 let resultSubtraction = subtract(10, 4);
 let resultMultiplication = multiply(2, 6);
 let resultDivision = divide(8, 2);
@@ -31,6 +51,10 @@ let resultDivisionByZero = divide(10, 0);
 
 // Print results to the console
 console.log("Result of addition:", resultAddition);
+console.log("Result of addition1:", resultAddition1);
+console.log("Result of addition2:", resultAddition2);
+
+
 console.log("Result of subtraction:", resultSubtraction);
 console.log("Result of multiplication:", resultMultiplication);
 console.log("Result of division:", resultDivision);
