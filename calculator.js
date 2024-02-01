afe1bf64aa566d8bce794d28ad26a04d5a2677d0
chandu160 <jrayala@albany.edu>
2024-01-31 21:13:15 -0500

    original position

--- calculator.js
+++ calculator.js
@@ -1,20 +1,17 @@
+// Addition
+function add(a, b) {
+    return a + b;
+}
+
 // Subtraction
 function subtract(a, b) {
     // return a - b;
 }
+
 // Multiplication
 function multiply(a, b) {
     // return a * b;
 }
-
-
-// Addition
-function add(a, b) {
-     return a + b;
-}
-
-
-
 
 // Division
 function divide(a, b) {
