2b7abafac9ad809a05dbc05f47b902611562e525
Samhitha Uppalapati <85285407+SAMHITHA0224@users.noreply.github.com>
2024-04-29 13:53:14 -0400

    fetched data from the api

--- src/Pages/Spotify/spotify.js
+++ src/Pages/Spotify/spotify.js
@@ -9,7 +9,7 @@ const Spotify = () => {
 
   const [searchParams] = useSearchParams();
   const emotion = searchParams.get("emotion");
-
+   
   const [categories, setCategories] = useState([]);
   const [playlists, setPlaylists] = useState([]);
   const [tracks, setTracks] = useState([]);
@@ -38,7 +38,7 @@ const Spotify = () => {
 
         // Check if there is an emotion query param and filter categories
         if (emotion && musicMoods[emotion]) {
-
+           
           const filteredCategories = items.filter((category) =>
             musicMoods[emotion].includes(category?.name)
           );
@@ -65,3 +65,51 @@ const Spotify = () => {
 
     fetchCategories();
   }, [token]);
+
+  const fetchPlaylists = async (categoryId, categoryName) => {
+    try {
+      const response = await axios.get(
+        `https://api.spotify.com/v1/browse/categories/${categoryId}/playlists`,
+        {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        }
+      );
+      setPlaylists(response.data.playlists.items);
+      setCurrentView("playlists");
+      // Update breadcrumbs to include the new category
+      setBreadcrumbs([
+        { name: "Categories", view: "categories" },
+        { name: categoryName, view: "playlists" },
+      ]);
+    } catch (error) {
+      console.error("Error fetching playlists:", error);
+    }
+  };
+
+  const fetchTracks = async (playlistId, playlistName) => {
+    try {
+      const response = await axios.get(
+        `https://api.spotify.com/v1/playlists/${playlistId}/tracks`,
+        {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        }
+      );
+      setTracks(response.data.items);
+      setCurrentView("tracks");
+      // Update breadcrumbs to include the new playlist
+      setBreadcrumbs(prevBreadcrumbs => [
+        ...prevBreadcrumbs.filter(crumb => crumb.view !== "tracks"), // Keep all but the 'tracks' breadcrumb
+        { name: playlistName, view: "tracks" },
+      ]);
+
+      // Extract URIs from the fetched tracks and update the trackUris state
+      const uris = response?.data?.items.map((item) => item?.track?.uri);
+      setTrackUris(uris);
+    } catch (error) {
+      console.error("Error fetching tracks:", error);
+    }
+  };
